d96f2d0ebffb4294ce8746d9056d8153d1cc4b28
DevVHO <161318358+DevVHO@users.noreply.github.com>
2024-06-03 13:19:30 +0100

    Lesson 3.3 - Don't Just Stand There
    Nesta [etapa eu estive aprendendo a colocar as animações a minha personagem

--- Assets/Course Library/Scripts/PlayerControl.cs
+++ Assets/Course Library/Scripts/PlayerControl.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     private Rigidbody bodyCOW;
+    private Animator playerAnim;
     public float jump = 10;
     public float GravityModifier = 2f;
     public bool isOnGround = true;
-    public bool Gameover = false ;
+    public bool Gameover = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,26 +21,25 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space ) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space ) && isOnGround && !Gameover)
         {
-            bodyCOW.AddForce(Vector3.up * 10f , ForceMode.Impulse);
+            bodyCOW.AddForce(Vector3.up * jump , ForceMode.Impulse);
+            playerAnim = GetComponent<Animator>();
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }       
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Ground"))
-        {
+        if(collision.gameObject.CompareTag("Ground")){
             isOnGround = true;
-        }
-        else if (collision.gameObject.CompareTag("Obstacle"))
-        {
-            Debug.Log("Gameover :(");
+
+        }else if (collision.gameObject.CompareTag("Obstacle")){
             Gameover = true;
+            Debug.Log("Gameover");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
-
-
-        
     }
 }
